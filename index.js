0ac9ffc2d87446958a449596caab64847ad54569
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 21:29:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 let dodger = document.getElementById("dodger");
+
 dodger.style.backgroundColor = "#FF6984";
+
+function moveDodgerLeft() {
+  
+}
+
+function moveDodgerRight() {
+  
+}
+
+document.addEventListener("keydown", function(pressed) {
+  if (pressed.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (pressed.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+})
