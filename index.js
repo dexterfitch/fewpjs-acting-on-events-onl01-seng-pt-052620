4355ca34325a243a5a3349d96642dbef89cc6995
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 21:39:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   let leftString = dodger.style.left.slice(0, -2);
   let leftNum = parseInt(leftString);
   if (leftNum > 0) {
-    dodger.style.left = `${leftNum - 3}px`;
+    dodger.style.left = `${leftNum - 1}px`;
   }
 }
 
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let leftString = dodger.style.left.slice(0, -2);
   let leftNum = parseInt(leftString);
   if (leftNum < (400 - dodger.offsetWidth)) {
-    dodger.style.left = `${leftNum + 3}px`;
+    dodger.style.left = `${leftNum + 1}px`;
   }
 }
 
