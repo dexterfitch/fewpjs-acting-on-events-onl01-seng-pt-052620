d55ddce2109887006c4f3cc11eff34f79f4d510a
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-08 21:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF6984";
 
 function moveDodgerLeft() {
-
+  let leftString = dodger.style.left.slice(0, -2);
+  let leftNum = parseInt(leftString);
+  if (leftNum > 0) {
+    dodger.style.left = `${leftNum - 1}px`;
+  }
 }
 
 function moveDodgerRight() {
